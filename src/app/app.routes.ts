a1a7d5ec0891a095b0389b34c61080200c0067e7
Antonio Jurić <juricantonio18@gmail.com>
2024-06-09 17:42:35 +0200

    Added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostCreateComponent } from './components/post-create/post-create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'posts',
+    component: PostListComponent,
+    title: 'Posts',
+  },
+  {
+    path: 'create',
+    component: PostCreateComponent,
+    title: 'Create',
+  },
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
