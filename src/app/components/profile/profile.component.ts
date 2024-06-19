106b97486aacf694ac39aa5260a46a1eef2ba58c
Antonio Jurić <juricantonio18@gmail.com>
2024-06-19 20:14:51 +0200

    Updated profile forms

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,28 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { mimeType } from "../../validators/mime-type.validator";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent implements OnInit {
+  isOwner = true;
   user!: User;
+
+  imageForm!: FormGroup;
+  imagePreview!: string;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.user = this.authService.getUser();
+
+    this.imageForm = new FormGroup({
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType],
+      }),
+    });
   }
 
   onSetUsername(form: NgForm) {
     console.log(form.value.username);
   }
 
-  onSetPhoto(form: NgForm) {
-    console.log(form.value.username);
+  onSetPhoto() {
+    
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      this.imageForm.patchValue({ image: file });
+      this.imageForm.get('image')?.updateValueAndValidity();
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
   }
 }
